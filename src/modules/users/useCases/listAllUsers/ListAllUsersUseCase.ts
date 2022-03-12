27a86bedb4ce1a5ca30a18f8f606679ae19bc546
NathalyaStefhany <nathalya.stefhany@hotmail.com>
2022-03-12 20:52:09 -0300

    feat: finished project

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) throw new Error("User not exists!");
+
+    const isAdmin = user.admin;
+
+    if (!isAdmin) throw new Error("User doesn't have permission!");
+
+    return this.usersRepository.list();
   }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,3 @@
 import { app } from ".";
 
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(4000, () => console.log("Server is running!"));
